c8ae01553cc721b9f6132585ddd938bbbf86c348
pazoler <56042404+pazoler@users.noreply.github.com>
2021-09-19 09:36:33 +0300

    Comment's
    Add comment's

--- Task1.java
+++ Task1.java
@@ -1,5 +1,6 @@
 public class Task1 {
     public static void main (String[] args){
+//Написать первую программу, которая выводит в консоль строку “Hello World!”.
         System.out.println("Hello World!");
     }
 }
